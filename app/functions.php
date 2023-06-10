2bd7cfebe79e73fb909b6d384f5cb84cbf5bc1e4
manabu <miyamotoma678@gmail.com>
2023-06-10 19:04:38 +0900

    今からWORKSページもデータベース化する

--- app/functions.php
+++ app/functions.php
@@ -57,8 +57,8 @@ function validateText($text)
 
 
 // データベースの定数
-// define('DSN', 'mysql:host=127.0.0.1;dbname=myblog;charset=utf8mb4');
-define('DSN', 'mysql:host=' . $_SERVER['HTTP_HOST'] . ';dbname=myblog;charset=utf8mb4');
+// define('DSN', 'mysql:host=127.0.0.1;dbname=mysite;charset=utf8mb4');
+define('DSN', 'mysql:host=' . $_SERVER['HTTP_HOST'] . ';dbname=mysite;charset=utf8mb4');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
